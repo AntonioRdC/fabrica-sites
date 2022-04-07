7e0f26b620db4c02e30ff6f431974f161e01c09e
AntonioRdC <antonioribeiroamg@gmail.com>
2022-04-06 22:26:38 -0400

    'certo'

--- core/models.py
+++ core/models.py
@@ -1,3 +1,34 @@
+from distutils.command import upload
+from statistics import mode
+from tkinter.tix import Tree
 from django.db import models
+from stdimage.models import StdImageField
 
-# Create your models here.
+
+class Base(models.Model):
+    criado = models.DateTimeField(auto_now_add=True)
+    modificado = models.DateTimeField(auto_now=True)
+    ativo = models.BooleanField(default=True)
+
+    class Meta:
+        abstract = True
+
+
+class Cargo(Base):
+    cargo = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name = 'Cargo'
+        verbose_name_plural = 'Cargos'
+
+    def __str__(self) -> str:
+        return self.cargo
+
+
+class Funcionario(Base):
+    nome = models.CharField('Nome', max_length=100)
+    biografia = models.TextField('Bio', max_length=1000)
+    cargo = models.ForeignKey('Cargo', on_delete=models.SET_NULL, null=True)
+    foto = StdImageField('Foto', upload_to='equipe', variations={'thumbnail': {'width': 600,
+                                                                                'height': 600,
+                                                                                'crop': True}})
--- fabricasites/settings.py
+++ fabricasites/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    
     'core',
 ]
 
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'fabricasites.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'fabricasites',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': 3306,
     }
 }
 
